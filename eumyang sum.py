6e42f491bc16a33ab22e6473f229e3aa42c1bcfb
jojothepizza <52461409+jo-cho@users.noreply.github.com>
2021-05-14 21:47:53 +0900

    3rd practice

---
+++ eumyang sum.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# solution
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer += -absolutes[i]
+    return answer
+
+# test
+
+absolutes = [4,7,12]
+signs = [True, False, True]
+
+print(solution(absolutes, signs))
